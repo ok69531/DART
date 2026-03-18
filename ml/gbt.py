f09c106588ad425a599bf1fc3cb5c749586303cd
조소영 <ok69531@gmail.com>
2026-03-18 16:56:24 +0900

    recon

--- ml/gbt.py
+++ ml/gbt.py
@@ -125,7 +125,7 @@ def main():
             result['rmse'][model_key].append(np.sqrt(mean_squared_error(fold_val_y, pred)))
             result['r2'][model_key].append(r2_score(fold_val_y, pred))
         
-        # save_results(result, path = f'saved_model/{args.assay_name}', file_name = f'quantile_{args.fp_type}_feat_sel_{args.use_feat_sel}.json')
+        save_results(result, path = f'saved_model/{args.assay_name}', file_name = f'quantile_{args.fp_type}_feat_sel_{args.use_feat_sel}.json')
             
     best_model_key, best_params, best_r2 = find_best_model(result, metric = 'r2')
     
